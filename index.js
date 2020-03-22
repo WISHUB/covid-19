85c32c6fc7e394b99d5992214e2927fc2561412f
Leandro Matilla <lmatilla@gen-it.com.ar>
2020-03-22 03:46:59 -0300

    ajustes de calculo

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export class Covid {
+class Covid {
 
     casos
     infectados_ayer
@@ -6,34 +6,31 @@ export class Covid {
 
     constructor(casos = [], infectados_hoy = undefined) {
         this.casos = casos
-        this.infectados_ayer = this.casos.length ? this.casos.slice(-1) : 0
+        this.infectados_ayer = this.casos.length ? this.casos[this.casos.length - 1] : 0
         this.infectados_hoy = infectados_hoy
     }
 
     get factores_de_riesgo() {
-        return this.casos.map((caso, index) => {
+        return this.casos.map((numero_casos, index) => {
             if (index === this.casos.length - 1)
-                return this.infectados_hoy ? this.infectados_hoy / caso : 0
+                return this.infectados_hoy ? this.infectados_hoy / numero_casos : 1
             else
-                return this.casos[index + 1] / caso
+                return this.casos[index + 1] / numero_casos
         })
     }
 
     get promedio_factores_de_riesgo() {
-        return this.factores_de_riesgo.length ?
-            this.factores_de_riesgo.reduce((anterior, actual) => actual += anterior) :
-            0
-    }
 
-    get factor_riesgo_global() {
-        return this.casos.length ?
-            this.promedio_factores_de_riesgo / this.casos.length :
-            0
+        const factores_length = this.factores_de_riesgo.length
+        const suma_factores_de_riesgo = this.factores_de_riesgo
+            .reduce((total, factor_riesgo) => total += factor_riesgo)
+
+        return factores_length ? suma_factores_de_riesgo / factores_length : 0
     }
 
     get nuevos_infectados() {
         return !this.infectados_hoy ?
-            Math.round(this.infectados_ayer * this.factor_riesgo_global) :
-            Math.round(this.infectados_hoy * this.factor_riesgo_global)
+            Math.round(this.infectados_ayer * this.promedio_factores_de_riesgo) :
+            Math.round(this.infectados_hoy * this.promedio_factores_de_riesgo)
     }
 }
