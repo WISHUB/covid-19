be1cdb5c071aa2d223d9fb07d21276e23705c8d6
Leandro Matilla <lmatilla@gen-it.com.ar>
2020-03-22 03:21:56 -0300

    se agregó un archivo extra con la información de casos y se agregaron validaciones a los calculos

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-class Covid {
+export class Covid {
 
     casos
     infectados_ayer
@@ -6,10 +6,10 @@ class Covid {
 
     constructor(casos = [], infectados_hoy = undefined) {
         this.casos = casos
-        this.infectados_ayer = this.casos.slice(-1)
+        this.infectados_ayer = this.casos.length ? this.casos.slice(-1) : 0
         this.infectados_hoy = infectados_hoy
     }
-  
+
     get factores_de_riesgo() {
         return this.casos.map((caso, index) => {
             if (index === this.casos.length - 1)
@@ -20,37 +20,20 @@ class Covid {
     }
 
     get promedio_factores_de_riesgo() {
-        return this.factores_de_riesgo.reduce((anterior, actual) => actual += anterior)
+        return this.factores_de_riesgo.length ?
+            this.factores_de_riesgo.reduce((anterior, actual) => actual += anterior) :
+            0
     }
 
     get factor_riesgo_global() {
-      return this.promedio_factores_de_riesgo / this.casos.length
+        return this.casos.length ?
+            this.promedio_factores_de_riesgo / this.casos.length :
+            0
     }
 
     get nuevos_infectados() {
-        return !this.infectados_hoy
-            ? Math.round(this.infectados_ayer * this.factor_riesgo_global)
-            : Math.round(this.infectados_hoy * this.factor_riesgo_global)
+        return !this.infectados_hoy ?
+            Math.round(this.infectados_ayer * this.factor_riesgo_global) :
+            Math.round(this.infectados_hoy * this.factor_riesgo_global)
     }
 }
-
-const casos = [
-    12,
-    17,
-    19,
-    21,
-    31,
-    34,
-    45,
-    56,
-    65,
-    79,
-    98,
-    128,
-    158,
-    225
-]
-
-const covid = new Covid(casos)
-
-console.log({ nuevos_infectados: covid.nuevos_infectados })
